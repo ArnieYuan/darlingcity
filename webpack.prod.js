c49143e9ddb34480ad493ec79b0a391621ebfd74
ArnieYuan <pkuying@gmail.com>
2026-07-02 22:24:00 -0700

    Update production configuration in webpack.prod.js

---
+++ webpack.prod.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    entry: {
+        main: './src/index.ts',
+        edit: './src/edit.ts'
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: 'auto',
+        filename: '[name].[contenthash].js',
+        clean: true, // Clean the output directory before emit
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: {
+                    loader: 'ts-loader',
+                    options: {
+                        compilerOptions: {
+                            declaration: false
+                        }
+                    }
+                },
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.md$/,
+                type: 'asset/source',
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    mode: 'production',
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'DarlingCity',
+            template: './src/index.html',
+            chunks: ['main'],
+        }),
+        new HtmlWebpackPlugin({
+            title: 'Edit ActivityScript',
+            template: './src/edit.html',
+            filename: 'edit/index.html',
+            chunks: ['edit'],
+        }),
+    ],
+};
